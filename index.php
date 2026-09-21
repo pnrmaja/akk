bf4d1bd1147d735e779c5d9bfbe7c2bc7b05d639
Ammer Márk <mhzeagle@gmail.com>
2026-09-21 12:36:21 +0200

    terkep

--- index.php
+++ index.php
@@ -6,4 +6,18 @@ require __DIR__ . '/includes/header.php';
     <h1>Szalézi Ágazati Képzőközpont</h1>
     <p>Üdvözöljük a Szalézi Ágazati Képzőközpont oldalán!</p>
 
+    <section class="terkep-szekcio">
+        <h2>Hol találsz minket?</h2>
+        <iframe
+            src="https://www.google.com/maps?q=1044+Budapest,+V%C3%A1ci+%C3%BAt+73&output=embed"
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            allowfullscreen></iframe>
+        <p>
+            <a class="terkep-link" href="https://www.google.com/maps?q=1044+Budapest,+V%C3%A1ci+%C3%BAt+73" target="_blank" rel="noopener noreferrer">
+                Megnyitás a Google Térképen
+            </a>
+        </p>
+    </section>
+
 <?php require __DIR__ . '/includes/footer.php'; ?>
