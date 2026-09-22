7c370b3d63cc63004d95d1bb7a28976786b763fa
Ammer Márk <mhzeagle@gmail.com>
2026-09-22 08:28:40 +0200

    footer placeholderekkel

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+</main>
+
+<footer class="lablec">
+    <div class="lablec-tartalom">
+        <h2>Szalézi Ágazati Képzőközpont</h2>
+        <p>1044 Budapest, Váci út 73.</p>
+        <p>Telefon: +36 1 234 5678</p>
+        <p>E-mail: info@szalezi-akk.hu</p>
+        <p class="lablec-jogok">&copy; <?= date('Y') ?> Szalézi AKK. Minden jog fenntartva.</p>
+    </div>
+</footer>
+
+</body>
+</html>
